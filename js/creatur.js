9e0e591d3a3aa6e83949c429e6493eae94461861
florisweb <github@florisweb.tk>
2019-08-10 10:33:15 -0500

    Implemented the creation of creatures and their brains.

--- js/creatur.js
+++ js/creatur.js
@@ -2,34 +2,66 @@
 
 
 
-function _creatur() {
-	let brain;
+function _creatur(_DNA) {
+	let brain = createBrain(_DNA.brain);
 
 	const This = {
 		energy: 100,
-		DNA: {
-			size: 1,
-			speed: 1,
-			r: 0,
-			g: 100,
-			b: 255,
-
-			eyeRange: 100,
-
-			brain: [
-			]
-		}
+		DNA: _DNA,
+		brain: brain
 	}
 
 	return This;
 
 
+	function createBrain(_brainDNA) {
+		let brainStructure = [3]; // inputs
+
+		let layers = Math.abs(Math.round(_brainDNA[0]));
 
 
-	
+		for (let l = 0; l < layers; l++)
+		{
+			let curLayerLength = Math.abs(Math.round(_brainDNA[l + 1]));
+			brainStructure.push(curLayerLength);
+		}
+
+		brainStructure.push(4); // outputs
+
+		let brain = new NeuralNetwork(brainStructure);
+		let brainData = _brainDNA.splice(layers + 1, _brainDNA.length);
+
+		return populateBrain(brain, brainData);
+	}
 
 
 
+	function populateBrain(_brain, _brainData) {
+		for (let l = 1; l < _brain.layers.length; l++)
+		{
+			let cLayer 	= _brain.layers[l];
+			cLayer.b 	= arraySplice(_brainData, cLayer.b.length);
 
+			for (let n = 0; n < cLayer.w.length; n++)
+			{
+				cLayer.w[n] = arraySplice(_brainData, cLayer.w[n].length);
+			}
+		}
+
+		return _brain;
+	}
 
 }
+
+
+function arraySplice(_array, _length) {
+	let arr = _array.splice(0, _length);
+	for (let i = 0; i < _length; i++)
+	{
+		if (arr[i]) continue;
+		arr[i] = 1 - Math.random() * 2;
+	}
+	return arr;
+}
+
+
--- js/main.js
+++ js/main.js
@@ -1,1 +1,43 @@
 
+const Main = new function() {
+
+	let This = {
+		creaturs: [],
+
+		createRandomCreatur: createRandomCreatur,
+		createCreatur: createCreatur
+	}
+
+	return This;
+
+
+	function createRandomCreatur() {
+		let DNA = {
+			size: 1,
+			speed: 1,
+			r: 255 * Math.random(),
+			g: 255 * Math.random(),
+			b: 255 * Math.random(),
+
+			eyeRange: 100,
+
+			brain: []
+		};
+
+
+		DNA.brain = [
+			Math.random()
+		];
+		for (let i = 0; i < DNA.brain[0]; i++) DNA.brain.push(Math.random() * 2);
+
+		return createCreatur(DNA);
+	}
+
+
+	function createCreatur(_DNA) {
+		let creatur = new _creatur(_DNA);
+		This.creaturs.push(creatur);
+		return creatur;
+	}
+
+}
